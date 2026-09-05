51476f9ba6efc6d653e6688876a3aef8f62a7f01
Aditya Singh <firstadityasingh@gmail.com>
2026-09-05 13:16:55 +0530

    task 7/50 complete

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -2,11 +2,17 @@ import express from 'express';
 import apiRouter from './routes/index.js';
 import { notFoundHandler } from './middleware/not-found.js';
 import { errorHandler } from './middleware/error-handler.js'; 
-
+import cors from 'cors';
 
 
 const app = express();
 
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.use('/api/v1', apiRouter);
